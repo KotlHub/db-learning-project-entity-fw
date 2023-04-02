4f7601eea711eedd052d7e7e29c443c511e3e826
RusHi PeaceYes <89914892+KotlHub@users.noreply.github.com>
2023-04-02 19:31:58 +0300

    Add files via upload

---
+++ WpfAdoProj/AdoProj/AdoProj/Insert.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AdoProj
+{
+    /// <summary>
+    /// Логика взаимодействия для Insert.xaml
+    /// </summary>
+    public partial class Insert : Window
+    {
+        wpfCrudEntities _db = new wpfCrudEntities();
+
+        public Insert()
+        {
+            InitializeComponent();
+        }
+
+        private void insertBtn_Click(object sender, RoutedEventArgs e)
+        {
+            member newMember = new member()
+            {
+                name = nametextBox.Text,
+                gender = gendercomboBox.Text
+            };
+
+            _db.members.Add(newMember);
+            _db.SaveChanges();
+            MainWindow.datagrid.ItemsSource = _db.members.ToList();
+            this.Hide();
+        }
+
+    }
+}
---
+++ WpfAdoProj/AdoProj/AdoProj/Update.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AdoProj
+{
+    /// <summary>
+    /// Логика взаимодействия для Update.xaml
+    /// </summary>
+    public partial class Update : Window
+    {
+        wpfCrudEntities _db = new wpfCrudEntities();
+        int Id;
+
+        public Update(int memberId)
+        {
+            InitializeComponent();
+            Id = memberId;
+        }
+
+        private void updateBtn_Click(object sender, RoutedEventArgs e)
+        {
+            member updateMember = (from m in _db.members
+                                   where m.id == Id
+                                   select m).Single();
+            updateMember.name = nametextBox.Text;
+            updateMember.gender = gendercomboBox.Text;
+            _db.SaveChanges();
+            MainWindow.datagrid.ItemsSource = _db.members.ToList();
+            this.Hide();
+        }
+    }
+}
